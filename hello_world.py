aa6539ecf12957d4c37a38d9d4e0548d9161aa5f
loglug1 <62907417+loglug1@users.noreply.github.com>
2024-01-19 11:40:51 -0600

    added a little program

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#namesake
+hello = "hello"
+world = "world"
+print(hello, world)
+
+#define your ranges
+start = 2
+end = 14
+
+for n in range(1,7):
+    delta_x = (end - start)/n
+    sum = 0
+    for i in range(0,n):
+        x_sub_i = start + (delta_x * i)
+        #function in terms of x_sub_i
+        y = 1 - 0.5*x_sub_i
+        sum += y*delta_x
+    print("Using " + str(n) + " rectangles's the area under the curve is " + str(sum))
